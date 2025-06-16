e38104b33f82d6c560193e641215d4704c38940c
raghunandan18 <raghunandan56565@gmail.com>
2025-06-16 11:22:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+const n = 25;
+const array= [];
+
+function init() {
+    array.length = 0; // Clear previous array
+    for (let i = 0; i < n; i++) {
+        const randomNumber = Math.floor(Math.random() * 100) + 1; // Random number between 1 and 100 
+        array.push(randomNumber);
+    }
+    showbars();
+}
+
+function startBubble() {
+    container.innerHTML = ''; // Clear previous bars
+    // init();
+    bubblesort(array);
+    showbars();
+}
+
+function startSelection() {
+    container.innerHTML = ''; // Clear previous bars
+    // init();
+    selectionSort(array);
+    showbars();
+}
+function startInsertion() {
+    container.innerHTML = ''; // Clear previous bars
+    // init();
+    insertionSort(array);
+    showbars();
+}
+
+
+function bubblesort(array){
+    do{
+        var swap = false;
+        for (let i = 0; i < array.length-1; i++) {
+            if (array[i] > array[i + 1]) {
+                // Swap elements
+                [array[i], array[i + 1]] = [array[i + 1], array[i]];
+                swap = true;
+            }
+        } 
+    }while (swap);
+}
+
+function selectionSort(array) {
+    do {
+        var swap = false;
+        for (let i = 0; i < array.length - 1; i++) {
+            let minIndex = i;
+            for (let j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex !== i) {
+                // Swap elements
+                [array[i], array[minIndex]] = [array[minIndex], array[i]];
+                swap = true;
+            }
+        }
+    } while (swap);
+}
+
+function insertionSort(array) {
+    do {
+        var swap = false;
+        for (let i = 1; i < array.length; i++) {
+            let key = array[i];
+            let j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+                swap = true;
+            }
+            array[j + 1] = key;
+        }
+    } while (swap);
+}
+
+function showbars() {
+    // const container = document.getElementById('container');
+    container.innerHTML = ''; // Clear previous bars
+    for (let i = 0; i < array.length; i++) {
+        const bar = document.createElement('div');
+        bar.style.height = array[i] * 5 +"px"; // Scale height for visibility  
+        bar.classList.add('bar');
+        container.appendChild(bar);
+    }
+}
